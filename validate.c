d566c583c20608d506c854b916afdda232bb0bba
macbook <macbook@macbooks-MacBook-Pro.local>
2024-11-16 03:43:21 +0100

    add: check for double chars

--- validate.c
+++ validate.c
@@ -6,7 +6,7 @@
 /*   By: macbook <macbook@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 00:03:56 by macbook           #+#    #+#             */
-/*   Updated: 2024/11/16 02:21:28 by macbook          ###   ########.fr       */
+/*   Updated: 2024/11/16 03:40:36 by macbook          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,31 +104,50 @@ int	collectible_exists(char **map)
 	return (0);
 }
 
+int	check_player_and_exit(char **map, char c)
+{
+	int	i;
+	int	j;
+	int	count;
+
+	i = 0;
+	j = 0;
+	count = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] == c)
+			{
+				count++;
+			}
+			j++;
+		}
+		i++;
+	}
+	if (count > 1)
+		return (1);
+	else
+		return (0);
+}
+
 int	validate_map(char **map)
 {
 	if (surrounded_walls(map, find_columns(map), find_rows(map)) < 1)
-	{
-		write(1, "Error\n", 6);
-		write(1, "Your Map is Not Surrounded by Walls\n", 38);
-		return (0);
-	}
+		return (write(1, "Error\n", 6), write(1,
+				"Your Map is Not Surrounded by Walls\n", 36), 0);
 	if (find_columns(map) < 3 || find_rows(map) < 3)
-	{
-		write(1, "Error\n", 6);
-		write(1, "Your Map is Not a Rectangle\n", 30);
-		return (0);
-	}
+		return (write(1, "Error\n", 6), write(1,
+				"Your Map is Not a Rectangle\n", 28), 0);
 	if (check_valid_path(map) < 1)
-	{
-		write(1, "Error\n", 6);
-		write(1, "Player Unable to Exit\n", 23);
-		return (0);
-	}
+		return (write(1, "Error\n", 6), write(1, "Player Unable to Exit\n", 22),
+			0);
 	if (collectible_exists(map) < 1)
-	{
-		write(1, "Error\n", 6);
-		write(1, "No collectible on the Map\n", 27);
-		return (0);
-	}
+		return (write(1, "Error\n", 6), write(1, "No collectible on the Map\n",
+				26), 0);
+	if (check_player_and_exit(map, 'E') || check_player_and_exit(map, 'P'))
+		return (write(1, "Error\n", 6), write(1,
+				"Map Contains Duplicate Characters\n", 34), 0);
 	return (1);
 }
